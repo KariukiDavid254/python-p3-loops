b8e72f6291944d0acb9cbd95958856a1a71a33c4
KariukiDavid254 <kariukidavidwangari254@gmail.com>
2023-05-21 10:21:23 +0300

    Looping

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    count = 10
+    while count >= 1:
+        print (count)
+        count -= 1
+        print ("Happy New Year!")
+happy_new_year()   
+
+def square_integers(numbers):
+    def square_numbers (numbers):
+     squared_numbers = [num ** 2 for num in numbers]
+     return squared_numbers
+
+    
     pass
 
-def square_integers(int_list):
-    # code goes here!
-    pass
 
 def fizzbuzz():
-    # code goes here!
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
     pass
